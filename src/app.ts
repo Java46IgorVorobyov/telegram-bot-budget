8fab8cca10a821e1e94f356ae42a3fa844494567
IgorrVvorobyov <95999049+IgorrVvorobyov@users.noreply.github.com>
2022-07-02 12:58:23 +0300

    init project

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.info('hi bro');
+import {Telegraf} from "telegraf";
+
+const bot = new Telegraf('5555214568:AAFWPC0q8evNcgsFBfHPcFIgsvQ3izp3I6s')
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hello broo'))
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
